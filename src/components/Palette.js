7d0091f079d1137756c4c87808ca13d4ce267767
tam <cherykim123@gmail.com>
2021-04-28 16:56:31 -0700

    added palette

---
+++ src/components/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="palette">
+        <div className="palette-colors"></div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
