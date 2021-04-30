a45b23f8f3458ed4f98637c7ee69a28481c86dfa
tam <cherykim123@gmail.com>
2021-04-29 22:25:44 -0700

    Add Select and Meterial-ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import Select from "@material-ui/core/Select";
+import { MenuItem } from "@material-ui/core";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "../style/Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+  }
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -22,6 +34,13 @@ class Navbar extends Component {
             step={100}
             onAfterChange={changeLevel}
           />
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
