5d6a706a2c074671ae3cc2eb8e728b85b6bdf38a
tam <cherykim123@gmail.com>
2021-05-03 13:45:34 -0700

    add Snackbar and styled select

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,25 @@ import React, { Component } from "react";
 import Select from "@material-ui/core/Select";
 import { MenuItem } from "@material-ui/core";
 import Slider from "rc-slider";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 import "rc-slider/assets/index.css";
 import "../style/Navbar.css";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: "hex" };
+    this.state = { format: "hex", open: false };
     this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
   handleFormatChange(e) {
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
+  }
+  closeSnackbar() {
+    this.setState({ open: false });
   }
   render() {
     const { level, changeLevel } = this.props;
@@ -42,6 +49,30 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed To {format.toUpperCase()}
+            </span>
+          }
+          contentProps={{
+            "aria-describedby": "message-id",
+          }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        ></Snackbar>
       </header>
     );
   }
