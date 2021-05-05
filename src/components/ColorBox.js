dfe892623be9466ba65d605b0abff419230dc7a5
tam <cherykim123@gmail.com>
2021-05-05 00:17:51 -0700

    add classNames

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
-import styles from "../JsStyle/ColorBoxStyle.js";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/styles";
+import styles from "../JsStyle/ColorBoxStyle";
 
 class ColorBox extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class ColorBox extends Component {
       setTimeout(() => this.setState({ copied: false }), 1500);
     });
   }
+
   render() {
     const {
       name,
@@ -23,20 +25,22 @@ class ColorBox extends Component {
       showingFullPalette,
       classes,
     } = this.props;
+
     const { copied } = this.state;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`${classes.copyOverlay} ${
-              copied && classes.showOverlay
-            }}`}
-          ></div>
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied,
+            })}
+          />
+
           <div
-            className={`${classes.copyMessage} ${
-              copied && classes.showMessage
-            }`}
+            className={classNames(classes.copyMessage, {
+              [classes.showMessage]: copied,
+            })}
           >
             <h1>copied!</h1>
             <p className={classes.copyText}>{background}</p>
@@ -57,5 +61,4 @@ class ColorBox extends Component {
     );
   }
 }
-
 export default withStyles(styles)(ColorBox);
