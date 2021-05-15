c07ec3893cb282c98964127f0c07ac413f3082b4
tam <cherykim123@gmail.com>
2021-05-14 21:34:59 -0700

    add deleteIcon to MiniPalette

--- src/JsStyle/MiniPaletteStyle.js
+++ src/JsStyle/MiniPaletteStyle.js
@@ -40,5 +40,18 @@ const styles = {
     position: "relative",
     marginBottom: "-3.5px",
   },
+  delete: {},
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0px",
+    top: "0px",
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0,
+  },
 };
 export default styles;
